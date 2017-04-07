137b7862f7a820dd8d656ce4ec4c13f50f453f5d
Ben Kaiser <strmbrkr00@aol.com>
2017-04-07 12:01:21 -0600

    more fleshed out run length

--- src/runlength.cpp
+++ src/runlength.cpp
@@ -1,10 +1,30 @@
+#include <iostream>
+
 using namespace std;
 
-const int TOLERANCERANGE = 16;
+const int TOLERANCERANGE = 4; // this is a decent change across all three channels
 
-void runlengthEncodeRange(char** image, int height, int width)
+struct pixel {
+	unsigned char red;
+	unsigned char green;
+	unsigned char blue;
+};
+
+void runlengthEncodeRange(pixel** image, int height, int width, bool isGrayscale)
 {
-	
+	unsigned char currentRunLength = 0;
+	pixel basePixel;  
+
+	for (int rowIndex = 0; rowIndex < height; rowIndex++)
+	{
+		for (int columnIndex = 0; columnIndex < width; rowIndex++)
+		{
+			if (abs(image[rowIndex][columnIndex].red - basePixel.red) > 4 && abs(image[rowIndex][columnIndex].green - basePixel.green) && abs(image[rowIndex][columnIndex].blue - basePixel.green))
+			{
+				
+			}
+		}
+	}
 }
 
 void runlengthDecodeRange()
@@ -18,4 +38,9 @@ void runlengthEncodeBitPlane(char** compress /*other params*/)
 void runlengthDecodeBitPlane()
 {
 	
+}
+
+int main()
+{
+	cout << "Hi" << endl;
 }
