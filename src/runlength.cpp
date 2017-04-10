5212f8daf5683ead7d4b7df6c857bf55f70f49e9
Ben Kaiser <strmbrkr00@aol.com>
2017-04-09 20:10:27 -0600

    runlength lossy work

--- src/runlength.cpp
+++ src/runlength.cpp
@@ -1,72 +1,77 @@
-#include <iostream>
+#include "runlength.h"
+#include <opencv2/highgui/highgui.hpp>
 
-using namespace std;
 
 const int TOLERANCERANGE = 4; // this is a decent change across all three channels
 
-struct pixel {
-	unsigned char red;
-	unsigned char green;
-	unsigned char blue;
-};
+enum colors {BLUE, GREEN, RED};
 
-// not sure how to 
-char** compressedImage = NULL;
+int imageTypeCheck(Mat image)
+{
+	int temp = image.type();
+	bool grayscale = false;
+	switch (temp)
+	{
+		case CV_8UC3:
+			cout << "RGB" << endl;
+			grayscale = false;
+			break;
+		case CV_8UC1:
+			cout << "grayscale" << endl;
+			grayscale = true;
+			break;
+		default:
+			cout << "um" << endl;
+			break;
+	};
+	return grayscale;
+}
+
+unsigned char** compressedImage = NULL;
 
 void writeToStream(unsigned char count, char* rgbValues, bool isGrayscale)
 {
 	if (isGrayscale)
 	{
+		// write a single byte
 	}
 	else
 	{
+		// write all three bytes
 	}
 }
 
-void runlengthEncodeRange(pixel** image, int height, int width, bool isGrayscale)
+void runlengthEncodeRange(Mat image, int height, int width, bool isGrayscale)
 {
 	unsigned char currentRunLength = 0;
-	pixel basePixel;  
-
+	Vec3b basePixel = {0, 0, 0};  
+	int count = 0;
 	for (int rowIndex = 0; rowIndex < height; rowIndex++)
 	{
-		for (int columnIndex = 0; columnIndex < width; rowIndex++)
+		for (int columnIndex = 0; columnIndex < width; columnIndex++)
 		{
-			if (abs(image[rowIndex][columnIndex].red - basePixel.red) > 4 && abs(image[rowIndex][columnIndex].green - basePixel.green) > 4 && abs(image[rowIndex][columnIndex].blue - basePixel.blue) > 4)
+			//cout << rowIndex << " " << columnIndex << endl;
+			if (abs(image.at<Vec3b>(rowIndex, columnIndex)[RED] - basePixel[RED]) > 4 && abs(image.at<Vec3b>(rowIndex, columnIndex)[GREEN] - basePixel[GREEN]) > 4 && abs(image.at<Vec3b>(rowIndex, columnIndex)[BLUE] - basePixel[BLUE]) > 4)
 			{
 				// TODO write data to compression stream
-				basePixel = image[rowIndex][columnIndex];
-			}
-			else
-			{
-				currentRunLength++;
-				if (currentRunLength == 255)
-				{
-					//TODO write data to compression stream
-					currentRunLength = 0;
-				}
+				count++;
+				if (rowIndex < 20)
+					cout << count << "occurred: " << rowIndex << " " << columnIndex << endl;
+				basePixel = image.at<Vec3b>(rowIndex, columnIndex);
 			}
 		}
 		// TODO write out count and value no matter what
 	}
 }
 
-void runlengthDecodeRange(char** compressedBytes, )
+void runlengthDecodeRange(char** compressedBytes, bool isGrayscale)
 {
 
-}
-
-void runlengthEncodeBitPlane(char** compress /*other params*/)
-{
-
-}
-
-void runlengthDecodeBitPlane()
-{
-	
 }
 
 int main()
 {
-	cout << "Hi" << endl;
+	Mat image = imread("/home/student/7285523/csc442final/Images/circles.png", -1);
+	bool grayscaleFlag = imageTypeCheck(image);
+	runlengthEncodeRange(image, image.rows, image.cols, grayscaleFlag);
 }
