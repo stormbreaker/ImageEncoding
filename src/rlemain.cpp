9ee6d1cf7024f6965c4562419c927b2dc9e828b9
Ben Kaiser <strmbrkr00@aol.com>
2017-04-22 11:30:16 -0600

    pushing rmse for scott

--- src/rlemain.cpp
+++ src/rlemain.cpp
@@ -1,4 +1,5 @@
 #include "runlength.h"
+#include <cmath>
 #include <opencv2/highgui/highgui.hpp>
 
 bool imageTypeCheck(Mat image)
@@ -22,16 +23,45 @@ bool imageTypeCheck(Mat image)
 	return grayscale;
 }
 
+double rootMeanSquare(Mat image1, Mat image2)
+{
+
+	double sum = 0;
+	double average;
+	int numberOfPixels = image1.rows * image1.cols;
+
+	if (image1.rows != image2.rows || image1.cols != image2.cols)
+	{
+		return -1;
+	}
+	for (int i = 0; i < image1.rows; i++)
+	{
+		for (int j = 0; j < image1.cols; j++)
+		{
+			double intensity1 = .299 * image1.at<Vec3b>(i, j)[2] + .587 * image1.at<Vec3b>(i, j)[1] + .114 * image1.at<Vec3b>(i, j)[0];
+			double intensity2 = .299 * image2.at<Vec3b>(i, j)[2] + .587 * image2.at<Vec3b>(i, j)[1] + .114 * image2.at<Vec3b>(i, j)[0];
+			sum += pow(intensity1 - intensity2, 2);
+		}
+	}
+	average = sum/numberOfPixels;
+	return sqrt(average);	
+}
+
 int main()
 {
 	Mat image = imread("/home/student/7285523/csc442final/images/binary.png", CV_LOAD_IMAGE_COLOR);
-	
-	//runlengthEncodeRange(image, image.rows, image.cols);
-	//runlengthDecodeRange();
-	
-	runlengthEncodeBitPlane(image, image.rows, image.cols);
-	runlengthDecodeBitPlane();
+	Mat decodedImage;	
 
+	//runlengthEncodeRange(image, image.rows, image.cols);
+	//decodedImage = runlengthDecodeRange();
+
+
+	runlengthEncodeBitPlane(image, image.rows, image.cols);
+	decodedImage = runlengthDecodeBitPlane();
+
+	//decodedImage = imread("/home/student/7285523/csc442final/decoded.png", CV_LOAD_IMAGE_COLOR);
+
+	cout << rootMeanSquare(image, decodedImage) << endl;
 
 	return 0;
 }
